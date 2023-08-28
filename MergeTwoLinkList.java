a594f79b6ec8040b4930883ca8b74481e19ebaf1
Luis Renteria <luis.renva@gmail.com>
2023-08-27 19:56:16 -0500

    adding merge link list

---
+++ MergeTwoLinkList.java
@@ -0,0 +1,7 @@
+// Given two sorted linked lists, merge them so that the resulting linked list is also sorted. 
+// Consider two sorted linked lists and the merged list below them as an example
+// LinkList = 4, 8, 15, 19   LinkList2 = 7, 9 , 10, 16
+
+class MergeTwoLinkList {
+
+}
